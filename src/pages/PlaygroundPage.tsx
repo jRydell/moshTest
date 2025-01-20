2937f7272996bbe7a33992b73a44860bce65d23f
jRydell <johannilsrydell89@gmail.com>
2025-01-20 11:36:47 +0100

    done

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,14 @@
+import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
+import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <Onboarding />;
+  return (
+    <ExpandableText
+      text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta consectetur architecto eveniet labore laborum fugiat porro consequuntur doloremque rerum deleniti ex, expedita repudiandae eos ducimus quae? Quam veniam ratione iste consequuntur saepe autem omnis laboriosam reprehenderit, nesciunt voluptatem natus libero, culpa necessitatibus mollitia. Maxime ducimus assumenda dolores numquam nulla, nobis labore amet possimus, doloremque ipsa laboriosam temporibus distinctio suscipit sit? Doloremque accusamus nihil cupiditate aut rerum tenetur, quasi sint dicta odio, tempora odit pariatur eligendi atque perspiciatis hic voluptatibus expedita error repudiandae nam explicabo, sunt quod eum? Nobis, sint distinctio blanditiis vel dolores atque magnam sed modi nihil provident? Quam!
+"
+    />
+  );
 };
 
 export default PlaygroundPage;
--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -10,4 +10,23 @@ describe("UserAccount", () => {
 
     expect(screen.getByText(user.name)).toBeInTheDocument;
   });
+
+  it("should render button if user is admin", () => {
+    const user: User = { id: 1, name: "Johan", isAdmin: true };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  });
+
+  it("should not render button if user is not admin", () => {
+    const user: User = { id: 1, name: "Johan", isAdmin: false };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.queryByRole("button");
+    expect(button).not.toBeInTheDocument();
+  });
 });
